f2d3d7e3fa62f6d52086898e6717f0ed73cd30db
shahin <shahinurislama@gmail.com>
2024-04-09 13:43:55 +0530

    added new changes

--- dummycli.sh
+++ dummycli.sh
@@ -3,46 +3,17 @@
 
 ### find all the valid region in aws 
 
-regions=$(aws ec2 describe-regions \ 
- --query Regions[].RegionName --output text)
+regions=$(aws ec2 describe-regions \
+ --query "Regions[].RegionName" --output text)
 
  ### find all instance ids of ec2 instances
 
- instance_ids=$(aws ec2 describe-instances \
-  --region "us-east-1" \
-   --filters "Name=tag:Env ,Values=Dev" \
-   --query Reservations[].Instances[].InstanceId --output text)
+ instance_ids=$(aws ec2 describe-instances --region "us-east-1" \
+  --filters "Name=tag:Env,Values=Dev" \
+   "Name=instance-state-name,Values=running,stopped)" \
+  --query "Reservations[].Instances[].InstanceId" --output text)
 
 for instance in $instance_ids; do
-aws ec2 terminate-instances --instance-ids \
-"$instance"
+aws ec2 terminate-instances --instance-ids "$instance >/dev/null"
 echo "deleted instance with id= "$instance""
 done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#subnets=$(aws ec2 describe-subnets \
- #--filters "Name=tag:Createdby, Values=cli" \
- #--query "Subnets[].SubnetId" --output text)
- #for subnet in $subnets; do
- #aws ec2 delete-subnet --subnet-id "$subnet"
- #echo "deleted subnet $subnet"
- #done
