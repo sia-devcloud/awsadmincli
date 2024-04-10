7b171663fb885de436b51fc782c39a11179c409d
shahin <shahinurislama@gmail.com>
2024-04-10 12:29:30 +0530

    added new changes

--- dummycli.sh
+++ dummycli.sh
@@ -10,19 +10,17 @@ regions=$(aws ec2 describe-regions \
  echo "checking region $region"
 
  ### find all instance ids of ec2 instances
-
-      instance_ids=$(aws ec2 describe-instances --region $region \
+ instance_ids=$(aws ec2 describe-instances --region $region \
       --filters "Name=tag:${tagName},Values=${tagValue}" \
          "Name=instance-state-name,Values=running,stopped)" \
       --query "Reservations[].Instances[].InstanceId" \
        --output text)
-
-        if [[ $instance_ids != "None" ]]; then
+      if [[ $instance_ids != "None" ]]; then
         for instance in $instance_ids; do
         echo "Following instances will be stopped:${instance_ids}"
-      aws ec2 stop-instances --instance-ids $instance_ids > /dev/null
-      echo "stopped instance with id= $instance_ids"
-      done
+        aws ec2 stop-instances --instance-ids $instance_ids > /dev/null
+        echo "stopped instance with id= $instance_ids"
+        done
       else
          echo "no instance"
       fi
