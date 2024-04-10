ee8290c24db5f2a01e86c536de6b04844fdd830c
shahin <shahinurislama@gmail.com>
2024-04-10 12:16:16 +0530

    added new changes

--- dummycli.sh
+++ dummycli.sh
@@ -19,9 +19,11 @@ regions=$(aws ec2 describe-regions \
              --region $region)
 
         if [[ $instance_ids != "None" ]]; then
+        for instance in $instance_ids; do
         echo "Following instances will be stopped:${instance_ids}"
       aws ec2 stop-instances --instance-ids $instance_ids > /dev/null
       echo "stopped instance with id= $instance_ids"
+      done
       else
          echo "no instance"
       fi
